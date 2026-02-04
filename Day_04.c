1faf97fba4bcfd29772607473f9d5279bca68f7a
ishitasingh8737 <singhishita2007@gmail.com>
2026-02-04 16:04:50 +0530

    code

---
+++ Day_04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+int n, i, left, right, temp;
+    scanf("%d", &n);
+int a[n];
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+left = 0;
+right = n - 1;
+    while (left < right) {
+        temp = a[left];
+        a[left] = a[right];
+        a[right] = temp;
+        left++;
+        right--;
+    }
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
