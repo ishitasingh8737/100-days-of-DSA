aac5e80d9724ac8c63b698e5c50014eeb49159f9
ishitasingh8737 <singhishita2007@gmail.com>
2026-02-05 23:28:23 +0530

    code

---
+++ Day_05.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+int p, q;
+int a[100], b[100];
+int i = 0, j = 0;
+scanf("%d", &p);
+for (i = 0; i < p; i++) {
+scanf("%d", &a[i]);
+}
+scanf("%d", &q);
+for (i = 0; i < q; i++) {
+scanf("%d", &b[i]);
+}
+i = 0; 
+j = 0;
+while (i < p && j < q) {
+if (a[i] < b[j]) {
+printf("%d ", a[i]);
+i++;
+} else {
+printf("%d ", b[j]);
+j++;
+    }
+}
+while (i < p) {
+printf("%d ", a[i]);
+i++;
+}
+while (j < q) {
+printf("%d ", b[j]);
+j++;
+}
+return 0;
+}
