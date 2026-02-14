acd8de0bbbdabe6b7de6efca1f12e9d5621d422a
ishitasingh8737 <singhishita2007@gmail.com>
2026-02-14 19:46:15 +0530

    code

---
+++ Day_14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+int n, i, j, flag = 1;
+scanf("%d", &n);
+int a[n][n];
+for(i = 0; i < n; i++)
+for(j = 0; j < n; j++)
+scanf("%d", &a[i][j]);
+for(i = 0; i < n; i++)
+for(j = 0; j < n; j++)
+if((i == j && a[i][j] != 1) || (i != j && a[i][j] != 0))
+flag = 0;
+if(flag)
+printf("Identity Matrix");
+else
+printf("Not an Identity Matrix");
+return 0;
+}
