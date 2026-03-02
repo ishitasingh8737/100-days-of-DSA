a69f126ec0802ba8cfc805b387ea4464faaad3d1
ishitasingh8737 <singhishita2007@gmail.com>
2026-03-02 12:04:08 +0530

    code

---
+++ Day_30.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+int coeff;
+int exp;
+struct Node* next;
+};
+struct Node* createNode(int c, int e) {
+struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+newNode->coeff = c;
+newNode->exp = e;
+newNode->next = NULL;
+return newNode;
+}
+struct Node* insert(struct Node* head, int c, int e) {
+struct Node* newNode = createNode(c, e);
+if (head == NULL) {
+return newNode;
+}
+struct Node* temp = head;
+while (temp->next != NULL) {
+temp = temp->next;
+    }
+temp->next = newNode;
+return head;
+}
+void display(struct Node* head) {
+    struct Node* temp = head;
+while (temp != NULL) {
+if (temp->exp == 0)
+    printf("%d", temp->coeff);
+else if (temp->exp == 1)
+    printf("%dx", temp->coeff);
+else
+    printf("%dx^%d", temp->coeff, temp->exp);
+
+if (temp->next != NULL)
+    printf(" + ");
+
+temp = temp->next;
+    }
+}
+int main() {
+int n, coeff, exp;
+struct Node* head = NULL;
+scanf("%d", &n);
+for (int i = 0; i < n; i++) {
+scanf("%d %d", &coeff, &exp);
+head = insert(head, coeff, exp);
+    }
+display(head);
+return 0;
+}
